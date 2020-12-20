ea728a5b90a73f72ac7b85515ce06a2f05e55aa8
Mike Rowland <mikejrowland@gmail.com>
2020-12-20 18:03:12 -0500

    Getting there

--- day20.go
+++ day20.go
@@ -37,7 +37,6 @@ func cornerSearch(tiles []*Tile, edgeMap map[string][]int) []int {
 }
 
 // returns the rotations for t1 where there's a match for t2
-// XXX need to handle flips better. need to determine both flip and rotations, not just rotation
 // edges are numbered like:
 //   __0__
 //  |     |
@@ -51,42 +50,21 @@ func cornerSearch(tiles []*Tile, edgeMap map[string][]int) []int {
 //  |_____|
 //     2
 //
-// And we return the number of degrees we have to spin t1 such that its matching side faces up.
-type tilematch struct {
+
+type orientation struct {
 	degrees int
 	flipped bool
 }
 
-func findTileMatches(t1, t2 *Tile, baseRot int) map[tilematch]bool {
-	if t2 == nil {
-		return map[tilematch]bool{
-			{0, true}: true, {90, true}: true, {180, true}: true, {270, true}: true,
-			{0, false}: true, {90, false}: true, {180, false}: true, {270, false}: true,
-		}
-	}
-	fmt.Println("----  matching", baseRot, t1.id, t2.id)
-	res := map[tilematch]bool{}
-	for i := 0; i < 4; i++ {
-		t1e := t1.edges[i]
-		for j := 0; j < 4; j++ {
-			t2e := t2.edges[j]
-			if t1e == reverse(t2e) {
-				fmt.Println("-  t1's edge", i, "matches t2's edge", j)
-				res[tilematch{(90*i + baseRot) % 360, false}] = true
-			}
-			if t1e == t2e {
-				fmt.Println("-  t1's edge", i, "matches t2's REVERSE edge", j)
-				res[tilematch{(90*i + baseRot + 180) % 360, true}] = true
-			}
-		}
-	}
-	fmt.Println("-  result", res)
-	return res
+type maptile struct {
+	id int
+	or orientation
+	t  *Tile
 }
 
 func seamonsterSearch(tiles []*Tile, edgeMap map[string][]int) {
 	maps := getTiledMaps(tiles, edgeMap)
-	maps = [][][]int{maps[0]}
+	//maps = [][][]maptile{maps[0]}
 	for cornerIdx, tileMap := range maps {
 		fmt.Printf("\nmap %v is...", cornerIdx)
 		for _, row := range tileMap {
@@ -101,86 +79,58 @@ func seamonsterSearch(tiles []*Tile, edgeMap map[string][]int) {
 		indexedTiles[t.id] = t
 	}
 	fmt.Println()
+	if false {
+		return
+	}
+	textTileLength := len(tiles[0].edges[0]) - 2
+	textSideLength := len(maps[0][0]) * textTileLength
 
 	// Ok, now we have the maps. Time to search the seas!!
 	for _, tileMap := range maps {
-		sideLength := len(tileMap)*(len(tiles[0].edges[0])-1) + 2
-		lines := make([][]string, sideLength)
+		lines := make([][]string, textSideLength)
 		for i := 0; i < len(tileMap); i++ {
 			tileRow := tileMap[i]
+			lineRowStart := i * textTileLength
 			for j := 0; j < len(tileRow); j++ {
-				fmt.Println("=== Placing tile", i, j)
-				tileId := tileRow[j]
-				tile := indexedTiles[tileId]
-				var top, bottom, left, right *Tile
-				if i-1 >= 0 {
-					top = indexedTiles[tileMap[i-1][j]]
-				}
-				if i+1 < len(tileMap) {
-					bottom = indexedTiles[tileMap[i+1][j]]
-				}
-				if j-1 >= 0 {
-					left = indexedTiles[tileMap[i][j-1]]
-				}
-				if j+1 < len(tileMap) {
-					right = indexedTiles[tileMap[i][j+1]]
-				}
-				// flips!!!
-				topMatches := findTileMatches(tile, top, 0)
-				bottomMatches := findTileMatches(tile, bottom, 180)
-				leftMatches := findTileMatches(tile, left, 270)
-				rightMatches := findTileMatches(tile, right, 90)
-				rot := 0
-				flipped := false
-				for rot < 360 {
-					_, ok1 := topMatches[tilematch{rot, false}]
-					_, ok2 := bottomMatches[tilematch{rot, false}]
-					_, ok3 := leftMatches[tilematch{rot, false}]
-					_, ok4 := rightMatches[tilematch{rot, false}]
-					if ok1 && ok2 && ok3 && ok4 {
-						flipped = false
-						break
+				//fmt.Println("=== Placing tile", i, j)
+				tileIdOr := tileRow[j]
+				tile := indexedTiles[tileIdOr.id]
+				// Apply rotation to lines
+				//Flip first
+				textLines := make([][]string, len(tile.raw))
+				for i := 0; i < len(tile.raw); i++ {
+					if tileIdOr.or.flipped {
+						textLines[i] = strings.Split(reverse(tile.raw[i]), "")
+					} else {
+						textLines[i] = strings.Split(tile.raw[i], "")
 					}
-					_, ok1 = topMatches[tilematch{rot, true}]
-					_, ok2 = bottomMatches[tilematch{rot, true}]
-					_, ok3 = leftMatches[tilematch{rot, true}]
-					_, ok4 = rightMatches[tilematch{rot, true}]
-					if ok1 && ok2 && ok3 && ok4 {
-						flipped = false
-						break
+				}
+				// Then rotate
+				rot := tileIdOr.or.degrees
+				for rot > 0 {
+					//fmt.Println("rot is", rot)
+					newLines := make([][]string, len(textLines))
+					for i := 0; i < len(textLines); i++ {
+						newLines[i] = make([]string, len(textLines[i]))
 					}
-					rot += 90
-				}
-				if rot < 0 || rot == 360 {
-					log.Fatal("no rotation found, matches:\n", topMatches, "\n", bottomMatches, "\n", leftMatches, "\n", rightMatches)
-				}
-				fmt.Println("= result placement: Rotation", rot, "flipped?", flipped)
-
-				// Add to lines
-				textLines := tile.raw
-				if rot == 0 {
-					// Top matches top tile, Right edge matches right tile, etc
-				} else if rot == 90 {
-					// Top edge matches right tile
-				} else if rot == 180 {
-					// Left edge (reversed) matches right tile
-				} else if rot == 270 {
-					// Bottom edge (reversed) matches right tile
+					n := len(textLines)
+					for i := 0; i < len(textLines); i++ {
+						for j := 0; j < len(textLines[i]); j++ {
+							newLines[i][j] = textLines[n-j-1][i]
+						}
+					}
+					textLines = newLines
+					rot -= 90
 				}
 
-				for r := 0; r < len(textLines); r++ {
-					textSize := len(textLines[r])
-					lineRowStart := i * textSize
-					textToAdd := textLines[r][1 : textSize-1]
-					if j == 0 {
-						textToAdd = string(textLines[r][0]) + textToAdd
-					}
-					if j == len(tileRow)-1 {
-						textToAdd = textToAdd + string(textLines[r][textSize-1])
-					}
-					lines[lineRowStart+r] = append(lines[lineRowStart+r], strings.Split(textLines[r], "")...)
+				for r := 1; r < len(textLines)-1; r++ {
+					line := textLines[r]
+					lines[lineRowStart+r-1] = append(lines[lineRowStart+r-1], line[1:len(line)-1]...)
 				}
 			}
+		}
+		for _, l := range lines {
+			fmt.Println(strings.Join(l, ""))
 		}
 		// Ok, we have the lines!! Now mark the sea monsters with O, then count the #
 		seaMonsterCount := 0
@@ -235,7 +185,11 @@ func seamonsterSearch(tiles []*Tile, edgeMap map[string][]int) {
 	}
 }
 
-func getTiledMaps(tiles []*Tile, edgeMap map[string][]int) [][][]int {
+func getTiledMaps(tiles []*Tile, edgeMap map[string][]int) [][][]maptile {
+	indexedTiles := map[int]*Tile{}
+	for _, t := range tiles {
+		indexedTiles[t.id] = t
+	}
 	type edgePair struct {
 		id1 int
 		id2 int
@@ -268,83 +222,216 @@ func getTiledMaps(tiles []*Tile, edgeMap map[string][]int) [][][]int {
 	corners := cornerSearch(tiles, edgeMap)
 
 	// 4 tile maps, with each corner at the top-left
-	maps := make([][][]int, 4)
-	for cornerIdx := 0; cornerIdx < 4; cornerIdx++ {
-
-		placed := map[int]bool{}
-		mapSize := int(math.Sqrt(float64(len(idsToNeighbors))))
-		tileMap := make([][]int, mapSize)
-		for i := 0; i < mapSize; i++ {
-			tileMap[i] = make([]int, mapSize)
-			for j := 0; j < mapSize; j++ {
-				if i == 0 && j == 0 {
-					tileMap[i][j] = corners[cornerIdx]
-					placed[corners[cornerIdx]] = true
-					continue
-				}
-				// Look at the neighbors of the tile to the left.
-				neighbors := map[int]bool{}
-				if i-1 < 0 {
-					for n, _ := range idsToNeighbors[tileMap[i][j-1]] {
-						neighbors[n] = true
+	var maps [][][]maptile
+	for cornerIdx := 0; cornerIdx < 1; cornerIdx++ {
+		for flipIdx := 1; flipIdx <= 1; flipIdx++ {
+			flipped := false
+			if flipIdx == 1 {
+				flipped = true
+			}
+			fmt.Println("\n===== corner", cornerIdx, "flip", flipped)
+			placed := map[int]bool{}
+			mapSize := int(math.Sqrt(float64(len(idsToNeighbors))))
+			tileMap := make([][]maptile, mapSize)
+			for i := 0; i < mapSize; i++ {
+				tileMap[i] = make([]maptile, mapSize)
+				for j := 0; j < mapSize; j++ {
+					fmt.Println("- placing", i, j)
+					if i == 0 && j == 0 {
+						id := corners[cornerIdx]
+						orient := getInitialOrientation(edgeMap, indexedTiles[id], flipped)
+						tileMap[i][j] = maptile{
+							id: id,
+							or: orient,
+							t:  indexedTiles[id],
+						}
+						placed[corners[cornerIdx]] = true
+						continue
 					}
-				} else if j-1 < 0 {
-					for n, _ := range idsToNeighbors[tileMap[i-1][j]] {
-						neighbors[n] = true
-					}
-				} else {
-					leftNeighbors := map[int]bool{}
-					upNeighbors := map[int]bool{}
-					for n, _ := range idsToNeighbors[tileMap[i][j-1]] {
-						leftNeighbors[n] = true
-					}
-					for n, _ := range idsToNeighbors[tileMap[i-1][j]] {
-						upNeighbors[n] = true
-					}
-					neighbors = leftNeighbors
-					// Get the set intersection (somewhat unwieldy)
-					for n, _ := range upNeighbors {
-						if _, ok := neighbors[n]; !ok {
-							delete(neighbors, n)
+					// Look at the neighbors of the tile to the left and up.
+					neighbors := map[int]bool{}
+					if i-1 < 0 {
+						for n, _ := range idsToNeighbors[tileMap[i][j-1].id] {
+							neighbors[n] = true
+						}
+					} else if j-1 < 0 {
+						for n, _ := range idsToNeighbors[tileMap[i-1][j].id] {
+							neighbors[n] = true
+						}
+					} else {
+						leftNeighbors := map[int]bool{}
+						upNeighbors := map[int]bool{}
+						for n, _ := range idsToNeighbors[tileMap[i][j-1].id] {
+							leftNeighbors[n] = true
+						}
+						for n, _ := range idsToNeighbors[tileMap[i-1][j].id] {
+							upNeighbors[n] = true
+						}
+						neighbors = leftNeighbors
+						// Get the set intersection (somewhat unwieldy)
+						for n, _ := range upNeighbors {
+							if _, ok := neighbors[n]; !ok {
+								delete(neighbors, n)
+							}
+						}
+						for n, _ := range leftNeighbors {
+							if _, ok := upNeighbors[n]; !ok {
+								delete(neighbors, n)
+							}
 						}
 					}
-					for n, _ := range leftNeighbors {
-						if _, ok := upNeighbors[n]; !ok {
-							delete(neighbors, n)
-						}
+					// Remove anything that's already been placed
+					for p, _ := range placed {
+						delete(neighbors, p)
 					}
-				}
-				// Remove anything that's already been placed
-				for p, _ := range placed {
-					delete(neighbors, p)
-				}
-				// Figure out how many neighbors this tile should have!
-				desiredNeighbors := 4
-				// Horizontal edge
-				if i == 0 || i == (mapSize-1) {
-					desiredNeighbors--
-				}
-				if j == 0 || j == (mapSize-1) {
-					desiredNeighbors--
-				}
-				for n, _ := range neighbors {
-					numNeighbors := len(idsToNeighbors[n])
-					if numNeighbors == desiredNeighbors {
-						tileMap[i][j] = n
-						placed[n] = true
-						break
+					// Figure out how many neighbors this tile should have!
+					desiredNeighbors := 4
+					// Horizontal edge
+					if i == 0 || i == (mapSize-1) {
+						desiredNeighbors--
+					}
+					if j == 0 || j == (mapSize-1) {
+						desiredNeighbors--
+					}
+					for n, _ := range neighbors {
+						numNeighbors := len(idsToNeighbors[n])
+						if numNeighbors == desiredNeighbors {
+							// Ok, we're POTENTIALLY placing this file here.
+							var upMaptile *maptile = nil
+							var leftMaptile *maptile = nil
+							if i-1 >= 0 {
+								upMaptile = &tileMap[i-1][j]
+							}
+							if j-1 >= 0 {
+								leftMaptile = &tileMap[i][j-1]
+							}
+							orientation, err := getOrientation(indexedTiles[n], upMaptile, leftMaptile)
+							if err != nil {
+								//Ok, no way to put it there.
+								fmt.Println("- no match for tile", n)
+								continue
+							}
+							fmt.Println("- match for tile", n, ":", orientation)
+							tileMap[i][j] = maptile{
+								id: n,
+								or: *orientation,
+								t:  indexedTiles[n],
+							}
+							placed[n] = true
+							break
+						}
 					}
 				}
 			}
+			maps = append(maps, tileMap)
 		}
-		maps[cornerIdx] = tileMap
 	}
 	return maps
 }
 
+func getInitialOrientation(edgeMap map[string][]int, tile *Tile, flipped bool) orientation {
+	// "Flips" will be considered as left-to-right
+	degrees := -1
+	// First find the two edges that face in.
+	topCt := len(edgeMap[tile.edges[0]])
+	rightCt := len(edgeMap[tile.edges[1]])
+	bottomCt := len(edgeMap[tile.edges[2]])
+	leftCt := len(edgeMap[tile.edges[3]])
+	if !flipped {
+		if topCt == 2 && rightCt == 2 {
+			degrees = 90
+		} else if rightCt == 2 && bottomCt == 2 {
+			degrees = 0
+		} else if bottomCt == 2 && leftCt == 2 {
+			degrees = 270
+		} else if leftCt == 2 && topCt == 2 {
+			degrees = 180
+		}
+	} else {
+		if topCt == 2 && rightCt == 2 {
+			degrees = 180
+		} else if rightCt == 2 && bottomCt == 2 {
+			degrees = 270
+		} else if bottomCt == 2 && leftCt == 2 {
+			degrees = 0
+		} else if leftCt == 2 && topCt == 2 {
+			degrees = 90
+		}
+	}
+	if degrees == -1 {
+		log.Fatal("ugh")
+	}
+
+	return orientation{
+		degrees: degrees,
+		flipped: flipped,
+	}
+}
+
+// Gets the facing edge
+func getEdge(startIdx int, mt *maptile) string {
+	idx := startIdx
+	turns := mt.or.degrees / 90
+	if !mt.or.flipped {
+		idx -= turns
+	} else {
+		idx += turns
+	}
+	if idx < 0 {
+		idx = idx + 4
+	}
+	idx = idx % 4
+	if mt.or.flipped {
+		return reverse(mt.t.edges[idx])
+	} else {
+		return mt.t.edges[idx]
+	}
+}
+
+func getOrientation(tile *Tile, up *maptile, left *maptile) (*orientation, error) {
+	//fmt.Println("--  Getting orientation for tile", tile.id)
+	var exposedUp, exposedLeft string
+	if up != nil {
+		exposedUp = getEdge(2, up)
+	}
+	if left != nil {
+		exposedLeft = getEdge(3, left)
+	}
+
+	//fmt.Println("--  up is", exposedUp, "left is", exposedLeft)
+	for i, e := range tile.edges {
+		//fmt.Println("--  edge", i, "is", e)
+		// Normal match: you have to REVERSE the one edge, since they're store in clockwise order
+		if reverse(e) == exposedUp || up == nil {
+			// Possible... check if the others match
+			leftEdgeIdx := (i - 1 + 4) % 4
+			if reverse(tile.edges[leftEdgeIdx]) == exposedLeft || left == nil {
+				//	fmt.Println("--  match 1, i=", i, ", leftEdgeIdx", leftEdgeIdx)
+				return &orientation{
+					degrees: i * 90,
+					flipped: false,
+				}, nil
+			}
+		}
+		// Check reverse match
+		if e == exposedUp || up == nil {
+			leftEdgeIdx := (i + 1 + 4) % 4
+			if tile.edges[leftEdgeIdx] == exposedLeft || left == nil {
+				//fmt.Println("--  match 2, i=", i, ", leftEdgeIdx", leftEdgeIdx)
+				return &orientation{
+					degrees: i * 90,
+					flipped: true,
+				}, nil
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("no match")
+}
+
 func main() {
 	lines, err := getLines("day20_dbg.txt")
-	//lines, err := getLines("day20_input.txt")
+	//	lines, err := getLines("day20_input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
